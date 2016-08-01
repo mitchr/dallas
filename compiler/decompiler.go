c3aef21223bf8c0f183bed46cd5fbbbc3c7d0df2
Mitchell-Riley <tug72074@temple.edu>
2016-08-01 11:15:25 -0400

    Handle lexing of 2-byte tokens in Decompile

--- compiler/decompiler.go
+++ compiler/decompiler.go
@@ -13,39 +13,57 @@ func Decompile(b []byte) ([]byte, []byte) {
 		}
 	}
 
-	// split string into 2 byte-size (kek) string chunks
-	// we want to convert to string here becaus adding two numbers will give us the sum, while adding two strings wqill concatenate them
+	// split string into 2 hexNum (1 byte) string chunks
+	// we want to convert to string here because adding two numbers will give us the sum, while adding two strings will concatenate them
 	splitB := []string{}
 	for i, j := 0, 1; j < len(b); i, j = i+2, j+2 {
 		splitB = append(splitB, string(b[i])+string(b[j]))
 	}
 
-	b = []byte{}
-	for _, v := range splitB {
-		c, _ := strconv.ParseUint(v, 16, 8)
-		b = append(b, byte(c))
+	for i := 0; i < len(splitB); i++ {
+		// first bytes of 2-byte tokens
+		if v := splitB[i]; v == "5d" || v == "5e" || v == "7e" || v == "aa" || v == "bb" {
+			splitB[i] += splitB[i+1]
+			splitB = append(splitB[:i+1], splitB[i+2:]...)
+		}
 	}
-	// remove last 2 elements from slice, aka the checksum
-	b = b[:len(b)-1]
-	b = b[:len(b)-1]
 
-	// offset is the beginning of the data section
+	cleanB := []uint16{}
+	for _, v := range splitB {
+		var c uint64
+		if len(v) == 2 {
+			c, _ = strconv.ParseUint(v, 16, 8)
+		} else if len(v) == 4 {
+			c, _ = strconv.ParseUint(v, 16, 16)
+		}
+		cleanB = append(cleanB, uint16(c))
+	}
+
+	// remove last 2 elements from slice, aka the checksum
+	cleanB = cleanB[:len(cleanB)-2]
+
 	const dataOffset = 0x37 + 0x11 + 0x02 // = 4a
 	const titleOffset = 0x37 + 0x05
 
 	var data []byte
-	for i := dataOffset; i < len(b); i++ {
-		if p, ok := revOneBytes[b[i]]; ok {
-			data = append(data, []byte(p)...)
-		}
+	for i := dataOffset; i < len(cleanB); i++ {
+		data = append(data, []byte(backwardsLex(cleanB[i]))...)
 	}
 
 	var title []byte
 	for i := titleOffset; i <= titleOffset+7; i++ {
-		if p, ok := revOneBytes[b[i]]; ok {
-			title = append(title, []byte(p)...)
-		}
+		title = append(title, []byte(backwardsLex(cleanB[i]))...)
 	}
 
 	return data, title
+}
+
+func backwardsLex(u uint16) string {
+	if p, ok := revTwoBytes[u]; ok {
+		return p
+	}
+	if p, ok := revOneBytes[byte(u)]; ok {
+		return p
+	}
+	return ""
 }
