e46e748ab61ea99f09812111d9c58b255d2f68d4
Mitchell-Riley <tug72074@temple.edu>
2016-11-05 14:47:19 -0400

    Simplify choosing of the output file name

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ import (
 var (
 	disAsm   = flag.Bool("d", false, "disassemble .8xp files")
 	progName = flag.String("p", "PROG", "set the program name")
-	outName  = flag.String("o", *progName+".8xp", "set the name of the output file")
 	archive  = flag.Bool("a", false, "set the archive bit; if false, ram is used to store the program")
 	lock     = flag.Bool("e", false, "set the edit-lock bit")
 	help     = flag.Bool("h", false, "display this help message")
@@ -41,13 +40,13 @@ func main() {
 	if *disAsm == true {
 		var b []byte
 		output, b = compiler.Decompile(inFile)
-		*progName = string(b)
-		*outName = *progName + ".tib"
+		*progName = string(b) + ".tib"
 	} else {
 		output = compiler.Compile(inFile, *progName, *archive, *ti83)
+		*progName += ".8xp"
 	}
 
-	outFile, err := os.Create(*outName)
+	outFile, err := os.Create(*progName)
 	if err != nil {
 		fmt.Println(err)
 	}
