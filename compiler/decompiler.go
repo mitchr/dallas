83455279a2b0046664552954b7f0efec56eed033
Mitchell-Riley <tug72074@temple.edu>
2017-08-04 10:28:43 -0400

    Refactor Decompiler
    Remove the strange behavior where we check the first byte of the
    compiled program. I wasn't able to find a compiler that replicated the
    behavior of writing the first byte as a '2' instead of '2a', and the two
    major compilers that I assume people use (Tokens and SourceCoder),
    including dallas, all write the first byte as '2a', and all successive
    bytes in such a fashion.
    
    Stop cleaning the program before we begin compilation. Binary files
    don't contain newlines or spaces, so we don't have to sanitize it before
    we can begin. Because we aren't cleaning anymore, this fixed the problem
    where 2 had to be subtracted from the dataOffset and titleOffset.
    
    Stop title decompilation if we find a 0x00 early.
    
    Rename split() to combine2ByteElements().
    
    backwardsLex() now returns an error along with the token slice.
    
    Add 0x63 to the list of 2-byte delimeters.

--- compiler/decompiler.go
+++ compiler/decompiler.go
@@ -1,98 +1,75 @@
 package compiler
 
-import "strconv"
+import (
+	"fmt"
+	"log"
+)
 
+// returns the decompiled program along with the title
 func Decompile(b []byte) ([]byte, []byte) {
-	// check if b was built from dallas, or some other compiler
-	// for some reason, other compilers spit out each byte as a pair of 2 ascii characters, where as dallas prints both characters together as an element of a []byte
-	cleanSource := make([]uint16, len(b))
-	// first byte is interpreted as "2"
-	if b[0] == 50 {
-		// split string into 2 hexNum (1 byte) string chunks
-		// we want to convert to string here because adding two numbers will give us the sum, while adding two strings will concatenate them
-		b = clean(b)
-
-		hexSource := []byte{}
-		for _, v := range string(b) {
-			c, _ := strconv.ParseUint(string(v), 16, 8)
-			hexSource = append(hexSource, byte(c))
-		}
-
-		// combine
-		splitSource := []byte{}
-		for i, j := 0, 1; j < len(hexSource); i, j = i+2, j+2 {
-			splitSource = append(splitSource, byte(hexSource[i]<<4)|byte(hexSource[j]))
-		}
-
-		cleanSource = split(splitSource)
-
-	} else if b[0] == 0x2a {
-		// first byte is interpreted as "2a", or "*" (compiled by dallas)
-		for i, v := range b {
-			cleanSource[i] = uint16(v)
-		}
-
-		cleanSource = split(clean(b))
-	}
-
-	// now we subtract 0x02 from each offset for some reason
-	// I legitimately do not know why, but I won't question it
-	const dataOffset = 0x37 + 0x11 + 0x02 - 0x02
-	const titleOffset = 0x37 + 0x05 - 0x02
+	const dataOffset = 0x37 + 0x11 + 0x02
+	const titleOffset = 0x37 + 0x05
 
 	var data []byte
-	// minus the checksum
-	for _, v := range cleanSource[dataOffset : len(cleanSource)-2] {
-		data = append(data, []byte(backwardsLex(v))...)
+	// dataOffset to the end minus the 2 bytes for the checksum
+	for _, v := range combine2ByteElements(b[dataOffset : len(b)-2]) {
+		k, err := backwardsLex(v)
+		if err != nil {
+			// should log.Fatal here once the token mapping is all figured out
+			fmt.Println(err)
+		}
+		data = append(data, []byte(k)...)
 	}
 
 	var title []byte
-	for _, v := range cleanSource[titleOffset : titleOffset+7] {
-		title = append(title, []byte(backwardsLex(v))...)
+	for _, v := range combine2ByteElements(b[titleOffset : titleOffset+7]) {
+		// if we find a 0x00, then the title is less than 8 characters
+		// so we can stop
+		if v == 0 {
+			break
+		}
+		k, err := backwardsLex(v)
+		if err != nil {
+			log.Fatal(err)
+		}
+		title = append(title, []byte(k)...)
 	}
 
 	return data, title
 }
 
-func clean(b []byte) []byte {
-	// remove spaces and line endings
-	// for Windows compatibility, check for "\r\n"
+// 2-byte elements are currently expressed as 2 separate elements.
+// (i.e. []uint16{0xbb01} is []byte{0xbb, 0x01})
+// loop through and combine these separate bytes into a single uint16
+func combine2ByteElements(b []byte) []uint16 {
+	// convert []byte to []uint16
+	source := make([]uint16, len(b))
 	for i, v := range b {
-		if v == ' ' || v == '\r' || v == '\n' {
-			b = append(b[:i], b[i+1:]...)
-		}
-	}
-	return b
-}
-
-func split(b []byte) []uint16 {
-	// 2-byte elements exist as 2 separate elements right now
-	// loop through and combine them so they exist as one element
-	splitSource := make([]uint16, len(b))
-	for i, v := range b {
-		splitSource[i] = uint16(v)
+		source[i] = uint16(v)
 	}
 
-	for i := 0; i < len(splitSource); i++ {
-		if is2ByteDelimeter(byte(splitSource[i])) {
-			splitSource[i] = (uint16(splitSource[i] << 8)) | uint16(splitSource[i+1])
-			splitSource = append(splitSource[:i+1], splitSource[i+2:]...)
+	for i := 0; i < len(source); i++ {
+		if is2ByteDelimeter(byte(source[i])) {
+			// combine the 2 elements
+			source[i] = (uint16(source[i] << 8)) | uint16(source[i+1])
+			// removed the 2 combined elements from the slice
+			source = append(source[:i+1], source[i+2:]...)
 		}
 	}
 
-	return splitSource
+	return source
 }
 
-func backwardsLex(u uint16) string {
+func backwardsLex(u uint16) (string, error) {
 	if p, ok := revTwoBytes[u]; ok {
-		return p
+		return p, nil
 	}
 	if p, ok := revOneBytes[byte(u)]; ok {
-		return p
+		return p, nil
 	}
-	return ""
+	return "", fmt.Errorf("token not found for key %#x", u)
 }
 
 func is2ByteDelimeter(b byte) bool {
-	return b == 0x5d || b == 0x5e || b == 0x7e || b == 0xaa || b == 0xbb
+	return b == 0x5d || b == 0x5e || b == 0x63 || b == 0x7e || b == 0xaa || b == 0xbb
 }
